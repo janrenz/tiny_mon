064787c13013a3cd14d9d1e509aa047d14b44138
jb <bresonik@bestfewo.de>
2015-04-17 15:39:47 +0200

    no message

--- app/steps/send_key_step.rb
+++ app/steps/send_key_step.rb
@@ -7,13 +7,13 @@ class SendKeyStep < Step
   def run!(session, check_run)
     symbol = :key.intern
     #(:scope.present?) ? element = session.find(:scope) : element = session.find('body')
-puts(:scope)
+puts(scope)
     element = session.find(:scope)
 
   rescue
     element = session.find('body')
   ensure
     element.native.send_key(symbol)
-    session.log "send key #{:key}"
+    session.log "send key #{key}"
   end
 end
