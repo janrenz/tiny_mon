8cf398de18162ef528a8064ccc905f19c85688b9
jb <bresonik@bestfewo.de>
2015-04-17 14:41:33 +0200

    added scope for send key feature

--- app/steps/send_key_step.rb
+++ app/steps/send_key_step.rb
@@ -1,12 +1,18 @@
 class SendKeyStep < Step
   property :key, :string
+  property :scope, :string
 
   validates_presence_of :key
 
   def run!(session, check_run)
     symbol = :key.intern
-    body = session.find('body')
-    body.native.send_key(symbol)
+    if :scope.present?
+      element = session.find(:scope)
+    else
+      element = session.find('body')
+    end
+
+    element.native.send_key(symbol)
     session.log "send key #{key}"
   end
 end
