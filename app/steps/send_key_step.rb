458902d077089d6de6916c5728cafa34eca759e7
jb <bresonik@bestfewo.de>
2015-04-17 15:37:28 +0200

    dirty testing...

--- app/steps/send_key_step.rb
+++ app/steps/send_key_step.rb
@@ -6,9 +6,14 @@ class SendKeyStep < Step
 
   def run!(session, check_run)
     symbol = :key.intern
-    (:scope.present?) ? element = session.find(:scope) : element = session.find('body')
+    #(:scope.present?) ? element = session.find(:scope) : element = session.find('body')
+puts(:scope)
+    element = session.find(:scope)
 
+  rescue
+    element = session.find('body')
+  ensure
     element.native.send_key(symbol)
-    session.log "send key #{key}"
+    session.log "send key #{:key}"
   end
 end
