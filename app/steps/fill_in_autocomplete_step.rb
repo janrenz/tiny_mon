b78e7873063dc8a02a8e40cf344272c458159e18
jb <bresonik@bestfewo.de>
2015-05-26 14:48:58 +0200

    changed class due typeahead update

--- app/steps/fill_in_autocomplete_step.rb
+++ app/steps/fill_in_autocomplete_step.rb
@@ -7,7 +7,7 @@ class FillInAutocompleteStep < FillInStep
   def run!(session, check_run)
     session.execute_script %Q{ $('##{field}').focus().typeahead('val', '').focus().typeahead('val', '#{value}'); }
 
-    session.find(".tt-suggestions")
+    session.find(".tt-dataset")
     session.execute_script %Q{ $(".tt-suggestion:contains('#{value} '):first").click();}
 
     session.log "field filled with #{value}"
